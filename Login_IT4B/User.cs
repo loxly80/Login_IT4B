708c0a3be679f3594538f907af0fa8f22cdc531e
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-10-24 08:59:18 +0200

    hash

--- Login_IT4B/User.cs
+++ Login_IT4B/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,9 +18,23 @@ namespace Login_IT4B
             Password = password;
         }
 
-        internal bool VerifyPassword(string text)
+        public bool VerifyPassword(string text)
         {
             return Password == text;
         }
+
+        private byte[] GetPasswordHash(string password)
+        {
+            byte[] passwordSalt;
+            byte[] passwordHash;
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+            return passwordHash;
+        }
+
+        
     }
 }
